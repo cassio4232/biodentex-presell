8860541cd57112afe0906775897f469fd28c9c57
cassio4232 <cassio130292@gmail.com>
2025-11-13 22:24:45 -0300

    Create js/config.js file with BioDentex affiliate configuration

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// BioDentex Affiliate Configuration
+
+const BioDentexConfig = {
+    clickBankHopLink: 'https://www.clickbank.net/?hop=biodentex',
+    productConfig: {
+        productId: '123456', // Replace with actual product ID
+        productName: 'BioDentex Teeth Whitening', // Replace with actual product name
+        price: 29.99, // Replace with actual product price
+    },
+    trackingSetup: {
+        enabled: true,
+        trackingId: 'UA-12345678-1', // Replace with actual tracking ID
+    }
+};
+
+export default BioDentexConfig;
